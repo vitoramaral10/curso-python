c17649f1f61b73dbfd3d4497292b9ae622cc2b42
Vitor <vitor.amaral10@gmail.com>
2026-04-16 21:34:49 -0300

    add input handling script for movie details

---
+++ fundamentos/3-input.py
@@ -0,0 +1,9 @@
+# Utilizando o input
+
+name = input("Digite o nome do filme:\n")
+yearLaunch = int(input("Digite o ano de lançamento do filme:\n"))
+noteMove = float(input("Digite a nota do filme:\n"))
+
+print(type(name))
+print(type(yearLaunch))
+print(type(noteMove))
