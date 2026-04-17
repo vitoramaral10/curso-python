d66c9029498de4c9527fb727e6cc8f5ce737eed9
Vitor <vitor.amaral10@gmail.com>
2026-04-16 21:27:07 -0300

    add Python script to display movie details and types

---
+++ fundamentos/2-dados.py
@@ -0,0 +1,15 @@
+# Pythonflix
+name = "Top Gun Maverick"
+yearLaunch = 2023
+noteMove = 9.5
+planIncluded = False
+
+print(name)
+print(yearLaunch)
+print(noteMove)
+print(planIncluded)
+
+print(type(name))
+print(type(yearLaunch))
+print(type(noteMove))
+print(type(planIncluded))
